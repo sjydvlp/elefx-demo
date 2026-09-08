c4e22b2815728d5002e4c4637a47e11ea4c3e30e
sjydvlp <sjydvlp@163.com>
2026-09-09 02:10:44 +0800

    feat：测试重构后的组件

--- src/main/java/com/sjydvlp/elefx/demo/controller/DemoController.java
+++ src/main/java/com/sjydvlp/elefx/demo/controller/DemoController.java
@@ -2,9 +2,15 @@ package com.sjydvlp.elefx.demo.controller;
 
 import cn.hutool.core.io.resource.ResourceUtil;
 import com.sjydvlp.elefx.component.button.EleFXButton;
+import com.sjydvlp.elefx.component.button.EleFXButtonSize;
 import com.sjydvlp.elefx.component.button.EleFXButtonType;
+import com.sjydvlp.elefx.component.icon.EleFXIcon;
 import com.sjydvlp.elefx.component.icon.EleFXIconType;
 import com.sjydvlp.elefx.component.icon.EleFXIcons;
+import com.sjydvlp.elefx.component.layout.EleFXCol;
+import com.sjydvlp.elefx.component.layout.EleFXRow;
+import com.sjydvlp.elefx.component.layout.EleFXRowAlign;
+import com.sjydvlp.elefx.component.layout.EleFXRowJustify;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -88,16 +94,19 @@ public class DemoController implements Initializable {
     }
 
     private void initializeLoader() {
+        // --- button
         HBox hBox1 = new HBox();
         hBox1.setPadding(new Insets(20));
         hBox1.setSpacing(10);
+        EleFXButton hBox1DefaultButton = new EleFXButton("Default", EleFXButtonType.DEFAULT, EleFXButtonSize.SMALL);
+        hBox1DefaultButton.setLoading(true);
         hBox1.getChildren().addAll(
-                new EleFXButton("Default"),
-                new EleFXButton("Primary", EleFXButtonType.PRIMARY),
-                new EleFXButton("Success", EleFXButtonType.SUCCESS),
-                new EleFXButton("Info", EleFXButtonType.INFO),
-                new EleFXButton("Warning", EleFXButtonType.WARNING),
-                new EleFXButton("Danger", EleFXButtonType.DANGER));
+                hBox1DefaultButton,
+                new EleFXButton("Primary", EleFXButtonType.PRIMARY, EleFXButtonSize.SMALL),
+                new EleFXButton("Success", EleFXButtonType.SUCCESS, EleFXButtonSize.SMALL),
+                new EleFXButton("Info", EleFXButtonType.INFO, EleFXButtonSize.SMALL),
+                new EleFXButton("Warning", EleFXButtonType.WARNING, EleFXButtonSize.SMALL),
+                new EleFXButton("Danger", EleFXButtonType.DANGER, EleFXButtonSize.SMALL));
 
         double iconSize = 18.0;
         
@@ -115,17 +124,43 @@ public class DemoController implements Initializable {
         HBox hBox3 = new HBox();
         hBox3.setPadding(new Insets(20));
         hBox3.setSpacing(10);
+        EleFXIcon hBox3PlusIcon = EleFXIcons.of(EleFXIconType.PLUS, iconSize);
+        hBox3PlusIcon.setLoading(true);
         hBox3.getChildren().addAll(
-                EleFXIcons.of(EleFXIconType.PLUS, iconSize),
+                hBox3PlusIcon,
                 EleFXIcons.of(EleFXIconType.MINUS, iconSize),
                 EleFXIcons.of(EleFXIconType.CIRCLE_PLUS, iconSize),
                 EleFXIcons.of(EleFXIconType.SEARCH, iconSize),
                 EleFXIcons.of(EleFXIconType.CHAT_LINE_SQUARE, iconSize),
                 EleFXIcons.of(EleFXIconType.CIRCLE_CLOSE, iconSize));
 
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll(hBox1, hBox2, hBox3);
+        // --- row
+        EleFXCol row1Left = new EleFXCol(12, new EleFXButton("左侧"));
+        EleFXCol row1Right = new EleFXCol(12, new EleFXButton("右侧"));
+        row1Left.setContentStyle("-fx-background-color: #2196F3;");
+        row1Right.setContentStyle("-fx-background-color: #fffaaa;");
+        EleFXRow row1 = new EleFXRow(20, row1Left, row1Right);
 
+        EleFXCol sidebar = new EleFXCol(6, new EleFXButton("侧栏"));
+        EleFXCol main = new EleFXCol(6, new EleFXButton("主内容"));
+        sidebar.setContentStyle("-fx-border-color: #2196F3;");
+        main.setContentStyle("-fx-border-color: #fffaaa;");
+        main.setOffset(6);
+        EleFXRow row2 = new EleFXRow(20, sidebar, main);
+
+        EleFXCol row3Left = new EleFXCol(6, new EleFXButton("A"));
+        EleFXCol row3Right = new EleFXCol(6, new EleFXButton("B"));
+        row3Left.setContentStyle("-fx-background-color: #aaafff;");
+        row3Right.setContentStyle("-fx-background-color: #cccccc;");
+        EleFXRow row3 = new EleFXRow(row3Left, row3Right);
+        row3.setJustify(EleFXRowJustify.SPACE_AROUND);
+        row3.setAlign(EleFXRowAlign.MIDDLE);
+
+        VBox vBox = new VBox();
+        vBox.setStyle("-fx-background-color: #ff0000;");
+        vBox.getChildren().addAll(hBox1, hBox2, hBox3, row1, row2, row3);
+
+        contentPane.setPadding(new Insets(30));
         contentPane.getChildren().add(vBox);
     }
 }
