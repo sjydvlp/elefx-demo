d306e3ebe432b7c9514b5030f6630a94451ceaab
sjydvlp <sjydvlp@163.com>
2026-09-19 22:31:17 +0800

    feat：测试EleFXTimeline组件

--- src/main/java/com/sjydvlp/elefx/demo/controller/DemoController.java
+++ src/main/java/com/sjydvlp/elefx/demo/controller/DemoController.java
@@ -91,6 +91,7 @@ import com.sjydvlp.elefx.component.text.EleFXText;
 import com.sjydvlp.elefx.component.text.EleFXTextSize;
 import com.sjydvlp.elefx.component.text.EleFXTextTag;
 import com.sjydvlp.elefx.component.text.EleFXTextType;
+import com.sjydvlp.elefx.component.timeline.*;
 import com.sjydvlp.elefx.component.transfer.EleFXTransfer;
 import com.sjydvlp.elefx.component.transfer.EleFXTransferItem;
 import com.sjydvlp.elefx.component.typography.EleFXTypography;
@@ -928,6 +929,22 @@ public class DemoController implements Initializable {
             tagHbox.getChildren().remove(tag5);
         });
 
+        // EleFXTimeline
+        EleFXTimelineItem timelineItem11 = new EleFXTimelineItem("Event start", "2018-04-15");
+        timelineItem11.setType(EleFXTimelineItemType.DANGER);
+        timelineItem11.setIcon(EleFXIcons.of(EleFXIconType.FOOTBALL, 20));
+//        timelineItem11.setColor("#ff0000");
+//        timelineItem11.setSize(EleFXTimelineItemSize.LARGE);
+        EleFXTimelineItem timelineItem12 = new EleFXTimelineItem("Approved", "2018-04-13");
+        timelineItem12.setHollow(true);
+        timelineItem12.setType(EleFXTimelineItemType.PRIMARY);
+        timelineItem12.setSize(EleFXTimelineItemSize.LARGE);
+        EleFXTimelineItem timelineItem13 = new EleFXTimelineItem("Success", "2018-04-11");
+        timelineItem13.setPlacement(EleFXTimelineItemPlacement.TOP);
+//        timelineItem13.setCenter(true);
+        EleFXTimeline timeline1 = new EleFXTimeline(timelineItem11, timelineItem12, timelineItem13);
+        timeline1.setMode(EleFXTimelineMode.ALTERNATE_REVERSE);
+//        timeline1.setReverse(true);
 
         // 添加
         VBox vBox = new VBox();
@@ -953,7 +970,7 @@ public class DemoController implements Initializable {
 //        vBox.getChildren().addAll(resultHbox);
 //        vBox.getChildren().addAll(skeleton1, skeleton2, skeleton3, skeleton4);
 //        vBox.getChildren().addAll(skeleton4, skeletonButton);
-        vBox.getChildren().addAll(tagHbox);
+        vBox.getChildren().addAll(tagHbox, timeline1);
 
         contentPane.setPadding(new Insets(30));
         contentPane.getChildren().add(vBox);
